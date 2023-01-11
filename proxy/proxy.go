175c57405e7e786dc995f7bdb2679d97473dabf3
sepehrsoh <sepehr.sobrabpour@carriot.ir>
2023-01-12 00:33:47 +0330

    handle multiple logs

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -2,12 +2,16 @@ package proxy
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"io"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"syscall"
 )
+
+var maxConn int = 0
 
 type ReversArgs struct {
 	LocalPort int
@@ -20,40 +24,45 @@ func NewReversArgs(localPort int, hostPort int, target string) *ReversArgs {
 }
 
 func ReverseProxy(args *ReversArgs) {
+	logrus.Infof("start server with target %v:%v \n Listen on port : %v", args.Target, args.HostPort, args.LocalPort)
 	incoming, err := net.Listen("tcp", fmt.Sprintf(":%d", args.LocalPort))
 	if err != nil {
 		log.Fatalf("could not start server on %d: %v", args.LocalPort, err)
 	}
 	fmt.Printf("server running on %d\n", args.LocalPort)
-	signals := make(chan os.Signal, 1)
-	stop := make(chan bool)
-	signal.Notify(signals, os.Interrupt)
-	signal.Notify(signals, os.Kill)
-	go func() {
-		for _ = range signals {
-			fmt.Println("\nStopping...")
-			stop <- true
+	c := make(chan os.Signal)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
+	for {
+		if maxConn < 10 {
+			maxConn += 1
+			defer func() {
+				maxConn -= 1
+			}()
+			go func() {
+				select {
+				case <-c:
+					logrus.Infoln("Shutting Down...")
+					return
+				default:
+					client, err := incoming.Accept()
+					if err != nil {
+						log.Fatal("could not accept client connection", err)
+					}
+					defer client.Close()
+
+					target, err := net.Dial("tcp", fmt.Sprintf("%v:%v", args.Target, args.HostPort))
+					if err != nil {
+						log.Fatal("could not connect to target", err)
+					}
+					defer target.Close()
+					go func() { io.Copy(target, client) }()
+					go func() { io.Copy(client, target) }()
+
+					<-c
+				}
+			}()
 		}
-	}()
 
-	client, err := incoming.Accept()
-	if err != nil {
-		log.Fatal("could not accept client connection", err)
 	}
-	defer client.Close()
-	fmt.Printf("client '%v' connected!\n", client.RemoteAddr())
-
-	target, err := net.Dial("tcp", fmt.Sprintf("%v:%v", args.Target, args.HostPort))
-	if err != nil {
-		log.Fatal("could not connect to target", err)
-	}
-	defer target.Close()
-	fmt.Printf("connection to server %v established!\n", target.RemoteAddr())
-	go func() { io.Copy(target, client) }()
-	go func() { io.Copy(client, target) }()
-	<-stop
-}
-
-func init() {
 
 }
